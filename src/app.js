d17424ab77e55a011e5f3d944ab57bb6d199d678
MoinakNaskar <119401674+MoinakNaskar@users.noreply.github.com>
2025-01-26 17:22:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(
-  cors({
-    origin: [process.env.CLIENT_URL, process.env.ADMIN_URL],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  cors(),
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
