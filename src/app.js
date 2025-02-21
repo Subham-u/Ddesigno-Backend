ba9329a864e0a967da43b75306cbeca70d4109e5
mainak naskar <mainaknaskar408@gmail.com>
2025-02-21 15:20:51 +0530

    payment added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(
-  cors(),
+  cors({
+    origin: [
+      process.env.CLIENT_URL,
+      process.env.ADMIN_URL,
+
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
